92c8bf99f786769d455d6c91af6c0cf3352dc1dc
Grażyna Skiba <grazyna.j.skiba@gmail.com>
2024-03-25 21:27:01 +0100

    logger for collecting execution time instead of cost by irace

--- src/main/scala/project/logging/AcoLogger.scala
+++ src/main/scala/project/logging/AcoLogger.scala
@@ -73,6 +73,26 @@ object AcoLogger {
     def close(): Unit = ()
   }
 
+  /**
+   * NOTE: Can be used to make irace treat time taken by the algorithm run as a result (might be useful for
+   * measuring execution time of the algorithm when testing configurations against test instances list).
+   */
+  class IraceStdOutTimeAsResult extends AcoLogger {
+    def config(problemConfig: ProblemConfig): Unit =
+      println(problemConfig)
+
+    def runTimeInfo(timeNano: Long): Unit =
+      println(s"${TimeUnit.NANOSECONDS.toMillis(timeNano)} ${TimeUnit.NANOSECONDS.toSeconds(timeNano)}")
+
+    def iterationResult(iteration: Int, result: Seq[BaseSolution]): Unit =
+      println((iteration, result.head.evaluation.head))
+
+    def globalBestResult(result: Seq[BaseSolution]): Unit = ()
+//      print(result.head.evaluation.head)
+
+    def close(): Unit = ()
+  }
+
   class StdOutAndFile(runId: String, writeToStdOut: Boolean, resultsWriter: PrintWriter)
       extends Basic(runId) {
 
